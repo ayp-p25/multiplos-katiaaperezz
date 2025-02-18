c937bb4b0f7c644b5a718a4622edaca6f39f0597
katiaaperezz <katiap.perez@iteso.mx>
2025-02-18 11:22:14 -0600

    Multiplos 8avo intento

--- multiplos.py
+++ multiplos.py
@@ -7,14 +7,14 @@ try:
     numero_2 = int(input("Introduzca otro número:"))
 #Proceso
     if numero_2 != 0:
-        if numero_1 % numero_2 == 0 :
+        if numero_1 % numero_2 == 0:
             resultado = "El número " + str(numero_1) + " es múltiplo del " + str(numero_2) 
         elif numero_2 % numero_1 == 0:
             resultado = "El número " +  str(numero_2) + " es múltiplo del " + str(numero_1) 
         else:
             resultado = "Ninguno de los números es múltiplo del otro"
-    else: numero_2 % numero_1 == 0
-    resultado= "El número " + str(numero_1) + " es múltiplo del " + str(numero_2) 
+    else: 
+        resultado= "El número " + str(numero_2) + " es múltiplo del " + str(numero_1) 
         
 except ValueError:
       resultado = "Introduzca otro valor."
