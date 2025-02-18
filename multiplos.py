6320cd51440fae239e369ad93365a030cd635461
katiaaperezz <katiap.perez@iteso.mx>
2025-02-18 11:13:20 -0600

    Multiplos 6to intento

--- multiplos.py
+++ multiplos.py
@@ -1,19 +1,22 @@
 """
 Multiplos
 """
-
+try:
 # Entradas
-numero_1 = int(input("Introduzca un número:"))
-numero_2 = int(input("Introduzca otro número:"))
-if numero_2 != 0:
-# Proceso
-    if numero_1 % numero_2 == 0 :
-        resultado = "El número " + str(numero_1) + " es múltiplo del " + str(numero_2) 
-    elif numero_2 % numero_1 == 0:
-        resultado = "El número " +  str(numero_2) + " es múltiplo del " + str(numero_1) 
-    else:
-        resultado = "Ninguno de los números es múltiplo del otro"
-else: numero_2 % numero_1
-resultado= "El número " + str(numero_2) + " es múltiplo del " + str(numero_1) 
+    numero_1 = int(input("Introduzca un número:"))
+    numero_2 = int(input("Introduzca otro número:"))
+#Proceso
+    if numero_2 != 0:
+        if numero_1 % numero_2 == 0 :
+            resultado = "El número " + str(numero_1) + " es múltiplo del " + str(numero_2) 
+        elif numero_2 % numero_1 == 0:
+            resultado = "El número " +  str(numero_2) + " es múltiplo del " + str(numero_1) 
+        else:
+            resultado = "Ninguno de los números es múltiplo del otro"
+    else: numero_2 % numero_1 == 0
+    resultado= "El número " + str(numero_1) + " es múltiplo del " + str(numero_2) 
+        
+except ValueError:
+      resultado = "Introduzca otro valor."
 # Salidas
 print(resultado)
